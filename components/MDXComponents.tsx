f1a2241f3d6cc9055ac45b64adba6bb6bd403106
realth000 <a598919335@gmail.com>
2024-10-20 23:33:39 +0800

    fix(build): entirely remove email api

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,6 +1,6 @@
 import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
-import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
+// import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
@@ -12,5 +12,5 @@ export const components: MDXComponents = {
   a: CustomLink,
   pre: Pre,
   table: TableWrapper,
-  BlogNewsletterForm,
+  // BlogNewsletterForm,
 }
